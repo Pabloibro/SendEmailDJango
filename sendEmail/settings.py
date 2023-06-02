26c1703fed04c8a42a905a32756033ce860da5c1
Ogundipe ibrahim <ogundipeibrahim1111@gmail.com>
2023-06-02 20:04:39 +0200

    final  commit

--- sendEmail/settings.py
+++ sendEmail/settings.py
@@ -123,9 +123,9 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.dummy.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True 
 EMAIL_POST = '587'
 EMAIL_HOST_USER = 'ogundipeibrahim19@gmail.com'
-EMAIL_HOST_PASSWORD = '######'
+EMAIL_HOST_PASSWORD = '#######'
