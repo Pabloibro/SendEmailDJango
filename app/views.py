6c3077551e9be8a0cd136c3a8916d8205e35b6dd
Ogundipe ibrahim <ogundipeibrahim1111@gmail.com>
2023-06-02 20:01:12 +0200

    #1 commit

--- app/views.py
+++ app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
 def home(request):
+    send_mail('Hey send from ibrahim Ogundipe',
+    'Hi, This mail is from Ibrahim Ogundipe',
+    'ogundipeibrahim19@gmail.com',
+    ['ogundipeibrahim1111@gmail.com',],
+    fail_silently=False)
     return render(request, 'home.html')
